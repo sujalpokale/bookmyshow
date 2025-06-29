0bd754824aba99be9f683058c4fffaf0aebf8e63
Sujal Pokale <156334934+sujalpokale@users.noreply.github.com>
2025-06-29 20:41:02 +0530

    Add files via upload

---
+++ src/Component/temp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import EntertaimentcardSider from "./Entertaimentcard/Entertailmentcard.components";
+function Homepage() {
+  return (
+  <>
+  <div className="container mx-auto px-4">
+    <h2 className="text-2xl font-bold text-gray-800"> The Best Of Live events </h2>
+    <EntertaimentcardSider/>
+    </div>
+  
+  </>
+)
+}
+
+export default Homepage;
